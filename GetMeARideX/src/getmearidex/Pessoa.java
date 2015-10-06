e0576dbba209a16b558c051bd0675f2b38375ba1
Corassa <ricardo_corassa@hotmail.com>
2015-10-06 14:01:15 -0300

    up Ricardo

---
+++ GetMeARideX/src/getmearidex/Pessoa.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package getmearidex;
+
+/**
+ *
+ * @author ricardo
+ */
+public class Pessoa {
+    
+    private String nome;
+    private String endereço;
+    private String Telefone;
+    private String email;
+    private String placa;
+    private String senha;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the endereço
+     */
+    public String getEndereço() {
+        return endereço;
+    }
+
+    /**
+     * @param endereço the endereço to set
+     */
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    /**
+     * @return the Telefone
+     */
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    /**
+     * @param Telefone the Telefone to set
+     */
+    public void setTelefone(String Telefone) {
+        this.Telefone = Telefone;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the placa
+     */
+    public String getPlaca() {
+        return placa;
+    }
+
+    /**
+     * @param placa the placa to set
+     */
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+}
